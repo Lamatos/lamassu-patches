ab281a6c767684f4b996278ee61ab3df5cc841d3
Lamatos <93525751+Lamatos@users.noreply.github.com>
2022-10-27 16:22:16 +0100

    Update eth-recovery-fix.sh

--- scripts/eth-recovery-fix.sh
+++ scripts/eth-recovery-fix.sh
@@ -4,7 +4,9 @@ echo
 echo "Patching lamassu-eth-recovery..."
 
 cd /bin && rm -rf lamassu-eth-recovery
-curl -#o $(npm root -g)/lamassu-server/bin/lamassu-eth-recovery https://raw.githubusercontent.com/josepfo/lamassu-server/104899c0ac6323cc8e0e6a39c7a88107a3cfb3e8/bin/lamassu-eth-recovery
+curl -#o $(npm root -g)/lamassu-server/bin/lamassu-eth-recovery https://raw.githubusercontent.com/lamassu/lamassu-server/v8.0.3/bin/lamassu-eth-recovery
+
+ln -s $(npm root -g)/lamassu-server/bin/lamassu-eth-recovery /usr/bin/lamassu-eth-recovery
 
 echo
 echo "Done. Please inform support that this script has been run."

d866f089b731f0cc1d383cee9835d6c4beee8d40
Lamatos <93525751+Lamatos@users.noreply.github.com>
2024-06-18 17:44:43 +0100

    Create manual_update.sh

---
+++ scripts/manual_update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set the URL for the update.tar file
+UPDATE_URL="https://fra1.digitaloceanspaces.com/lama-images/aaeon-upboard/Packages/update.tar?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=EDYKHSFKASPKKZH6WKGM%2F20240618%2Ffra1%2Fs3%2Faws4_request&X-Amz-Date=20240618T164243Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=7eb5f21ccb3e244426c902b058350a6e9159e68cbf566beadf7c9375b23a8d67"
+
+# Download the update.tar file
+curl -O $UPDATE_URL
+
+# Extract the update.tar file
+tar -xf update.tar
+
+# Change directory to the extracted update directory
+cd update
+
+# Extract the subpackage.tgz file
+tar -xf subpackage.tgz
+
+# Change directory to the subpackage directory
+cd subpackage
+
+# Copy the lamassu-machine directory to /opt
+cp -r lamassu-machine /opt
+
+echo "Update process completed successfully."

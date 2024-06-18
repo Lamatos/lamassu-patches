49b1051a97a4268eddfcb995bdf961d94ed8b859
Lamatos <93525751+Lamatos@users.noreply.github.com>
2024-06-18 17:57:35 +0100

    Update manual_update.sh

--- scripts/manual_update.sh
+++ scripts/manual_update.sh
@@ -3,11 +3,11 @@
 # Set the URL for the update.tar file
 UPDATE_URL="https://fra1.digitaloceanspaces.com/lama-images/aaeon-upboard/Packages/update.tar?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=EDYKHSFKASPKKZH6WKGM%2F20240618%2Ffra1%2Fs3%2Faws4_request&X-Amz-Date=20240618T164243Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=7eb5f21ccb3e244426c902b058350a6e9159e68cbf566beadf7c9375b23a8d67"
 
-# Download the update.tar file using curl with -J -L -O options
-curl -J -L -O "$UPDATE_URL"
+# Download the update.tar file using wget
+wget -q "$UPDATE_URL" -O update.tar
 
 # Check if the download was successful
-if [ ! -f update.tar ]; then
+if [ $? -ne 0 ]; then
     echo "Failed to download update.tar"
     exit 1
 fi
@@ -16,7 +16,7 @@ fi
 tar -xf update.tar
 
 # Check if the extraction was successful
-if [ ! -d update ]; then
+if [ $? -ne 0 ]; then
     echo "Failed to extract update.tar"
     exit 1
 fi
@@ -28,7 +28,7 @@ cd update || { echo "Failed to change directory to update"; exit 1; }
 tar -xf subpackage.tgz
 
 # Check if the extraction was successful
-if [ ! -d subpackage ]; then
+if [ $? -ne 0 ]; then
     echo "Failed to extract subpackage.tgz"
     exit 1
 fi
@@ -40,7 +40,7 @@ cd subpackage || { echo "Failed to change directory to subpackage"; exit 1; }
 cp -r lamassu-machine /opt
 
 # Check if the copy was successful
-if [ ! -d /opt/lamassu-machine ]; then
+if [ $? -ne 0 ]; then
     echo "Failed to copy lamassu-machine to /opt"
     exit 1
 fi

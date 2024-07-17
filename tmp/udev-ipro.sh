6f5e19964d43d0421503da0d6af107025643f076
Lamatos <93525751+Lamatos@users.noreply.github.com>
2024-07-17 15:34:53 +0100

    Create udev-ipro.sh

---
+++ tmp/udev-ipro.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the URL of the file and the destination path
+FILE_URL="https://github.com/lamassu/lamassu-machine/blob/1bf37b157f73fa1facfdaed4e22995783ff71479/hardware/system/coincloud/jcm-ipro-rc/udev/99-jcm-ipro-rc.rules"
+DEST_PATH="/etc/udev/rules.d/99-jcm-ipro-rc.rules"
+
+# Download the file and place it in the destination path
+curl -o "$DEST_PATH" -L "$FILE_URL"
+
+# Reload the udev rules
+udevadm control --reload-rules
+udevadm trigger
+
+# Print done message
+echo "Done!"

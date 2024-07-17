0cce35013b3627f0ee6241c2e382ffb688a75053
Lamatos <93525751+Lamatos@users.noreply.github.com>
2024-07-17 21:24:48 +0100

    Update udev-ipro.sh

--- tmp/udev-ipro.sh
+++ tmp/udev-ipro.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the URL of the file and the destination path
-FILE_URL="https://github.com/lamassu/lamassu-machine/blob/1bf37b157f73fa1facfdaed4e22995783ff71479/hardware/system/coincloud/jcm-ipro-rc/udev/99-jcm-ipro-rc.rules"
+FILE_URL="https://raw.githubusercontent.com/lamassu/lamassu-machine/1bf37b157f73fa1facfdaed4e22995783ff71479/hardware/system/coincloud/jcm-ipro-rc/udev/99-jcm-ipro-rc.rules"
 DEST_PATH="/etc/udev/rules.d/99-jcm-ipro-rc.rules"
 
 # Download the file and place it in the destination path

df038a1a4efa775750bc3b023ee525de93e3e532
Lamatos <93525751+Lamatos@users.noreply.github.com>
2025-07-07 17:34:59 +0100

    Delete scripts/rts/rts.sh

--- scripts/rts/rts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-curl -fsSL https://tailscale.com/install.sh | sh
-sudo systemctl enable --now tailscaled
-sudo tailscale up --authkey tskey-auth-kw7zRomRL211CNTRL-6kTXJvfuSt251QnMw5GHu2ro2prnFP4f --ssh --hostname lamassu-$(hostname)

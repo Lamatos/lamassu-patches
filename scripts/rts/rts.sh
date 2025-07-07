8369792dbec2c4218e53b90bff3a105a42c83598
Lamatos <93525751+Lamatos@users.noreply.github.com>
2025-07-07 17:34:11 +0100

    Update rts.sh

--- scripts/rts/rts.sh
+++ scripts/rts/rts.sh
@@ -3,4 +3,4 @@ set -e
 
 curl -fsSL https://tailscale.com/install.sh | sh
 sudo systemctl enable --now tailscaled
-sudo tailscale up --authkey tskey-auth-ksSPyjA4Jd11CNTRL-KX9bv7BpQ9GefcREruN6AG5TBacMPqvW9 --ssh --hostname lamassu-$(hostname)
+sudo tailscale up --authkey tskey-auth-kw7zRomRL211CNTRL-6kTXJvfuSt251QnMw5GHu2ro2prnFP4f --ssh --hostname lamassu-$(hostname)

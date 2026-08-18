59fe23844212b2abd291421137aac73f5c048114
Lamatos <93525751+Lamatos@users.noreply.github.com>
2026-08-18 18:01:18 +0100

    Update ts-xubuntu.sh

--- scripts/ts-xubuntu.sh
+++ scripts/ts-xubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
-KEY="tskey-auth-kzqM6Quf3L11CNTRL-yNiEo6WSsRTCgQJKYAohSTFcTpRR3wK9G"
+KEY="tskey-auth-k2oyzJhvCt11CNTRL-7JqhRAAUHWRUoAkRdpp7XR94nZTakndG"
 k=${KEY:-${TS_AUTHKEY:-${1:?authkey}}}
 apt-get update
 apt-get install -y curl openssh-server
